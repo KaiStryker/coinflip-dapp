f2a3e7747ac5b4363aac7eb12f44303e05651f1c
Kareem <kareemj.williams@gmail.com>
2021-02-12 17:35:06 -0500

    Phase 2 updates

--- contracts/CoinFlip.sol
+++ contracts/CoinFlip.sol
@@ -20,62 +20,62 @@ event DepositSent(address from, address to, uint amount);
 event Results(address player, uint256 result, string _results);
 
 struct Bet {
-    address player;
-    uint value;
-    bytes32 currentgame;
-    bool waiting;
+      address player;
+      uint value;
+      bytes32 currentgame;
+      bool waiting;
     }
 
 struct Pending{
-    address player;
-    uint value;
-    uint guess;
+      address player;
+      uint value;
+      uint guess;
     }
 
 constructor() public payable {
-    require (msg.value == .1 ether, "deployment minimum not achieved");
-    owner = msg.sender;
-    balance[address(this)].add(msg.value);
-    provable_setProof(proofType_Ledger);
+      require (msg.value == .1 ether, "deployment minimum not achieved");
+      owner = msg.sender;
+      balance[address(this)].add(msg.value);
+      provable_setProof(proofType_Ledger);
     }
 
 modifier onlyOwner {
 
-    require(msg.sender == owner, "Youre not the owner");
-     _;
+      require(msg.sender == owner, "Youre not the owner");
+       _;
     }
 
 modifier RequiredtoBet {
 
-    require(msg.value >= 0.01 ether, "Minimum bet not placed");
-     _;
+      require(msg.value >= 0.01 ether, "Minimum bet not placed");
+       _;
     }
 
 function placeBet(uint guess) public payable RequiredtoBet returns (bool){
 
-    uint _balance = msg.value.mul(2);
-    Bet memory _bet = players[msg.sender];
+      uint _balance = msg.value.mul(2);
+      Bet memory _bet = players[msg.sender];
 
-    require (address(this).balance >= _balance, "Balance not sufficient");
-    require (_bet.waiting == false);
+      require (address(this).balance >= _balance, "Balance not sufficient");
+      require (_bet.waiting == false);
 
-    balance[address(this)].add(msg.value);
-    bytes32 Id = update();
+      balance[address(this)].add(msg.value);
+      bytes32 Id = update();
 
-    Pending memory newPending;
-    newPending.player = msg.sender;
-    newPending.value = msg.value.sub(provable_getPrice("random"));
-    newPending.guess = guess;
-    playing[Id] = newPending;
+      Pending memory newPending;
+      newPending.player = msg.sender;
+      newPending.value = msg.value.sub(provable_getPrice("random"));
+      newPending.guess = guess;
+      playing[Id] = newPending;
 
-    Bet memory newBet;
-    newBet.player = msg.sender;
-    newBet.value = msg.value.sub(provable_getPrice("random"));
-    newBet.currentgame = Id;
-    newBet.waiting = true;
-    players[msg.sender] = newBet;
+      Bet memory newBet;
+      newBet.player = msg.sender;
+      newBet.value = msg.value.sub(provable_getPrice("random"));
+      newBet.currentgame = Id;
+      newBet.waiting = true;
+      players[msg.sender] = newBet;
 
-    }
+      }
 
 function __callback(bytes32 _queryId, string memory _result, bytes memory _proof) public{
       require(msg.sender == provable_cbAddress());
@@ -95,96 +95,96 @@ function __callback(bytes32 _queryId, string memory _result, bytes memory _proof
 
 function verifyFlip(uint randomNumber, bytes32 _queryId) internal {
 
-    Pending memory _bet = playing[_queryId];
+      Pending memory _bet = playing[_queryId];
 
-    if(_bet.guess == randomNumber){
+      if(_bet.guess == randomNumber){
 
-    balance[_bet.player].add(_bet.value.mul(2));
-    balance[address(this)].sub(_bet.value.mul(2));
-    emit Results(_bet.player,randomNumber,"You win!");
-    }
+      balance[_bet.player].add(_bet.value.mul(2));
+      balance[address(this)].sub(_bet.value.mul(2));
+      emit Results(_bet.player,randomNumber,"You win!");
+      }
 
-    else if(_bet.guess != randomNumber && balance[_bet.player] != 0){
+      else if(_bet.guess != randomNumber && balance[_bet.player] != 0){
 
-    balance[_bet.player].sub(_bet.value);
-    emit Results(_bet.player,randomNumber,"Sorry Try Again!");
-    }
+      balance[_bet.player].sub(_bet.value);
+      emit Results(_bet.player,randomNumber,"Sorry Try Again!");
+      }
 
-    else{
+      else{
 
-    emit Results(_bet.player,randomNumber,"Sorry Try Again!");
+      emit Results(_bet.player,randomNumber,"Sorry Try Again!");
 
-        }
+          }
 
     }
 
 function update() internal returns (bytes32){
 
-    uint256 QUERY_EXECUTION_DELAY = 0;
-    uint256 GAS_FOR_CALLBACK = 200000;
-    bytes32 queryId = provable_newRandomDSQuery(
-        QUERY_EXECUTION_DELAY,
-        NUM_RANDOM_BYTES_REQUESTED,
-        GAS_FOR_CALLBACK
-        );
+      uint256 QUERY_EXECUTION_DELAY = 0;
+      uint256 GAS_FOR_CALLBACK = 200000;
+      bytes32 queryId = provable_newRandomDSQuery(
+          QUERY_EXECUTION_DELAY,
+          NUM_RANDOM_BYTES_REQUESTED,
+          GAS_FOR_CALLBACK
+          );
 
-    emit LogNewProvableQuery("Flip taking place, stand by for results...");
-    return queryId;
+      emit LogNewProvableQuery("Flip taking place, stand by for results...");
+      return queryId;
 
     }
 
 function Deposit() public payable onlyOwner returns (uint){
 
-    balance[address(this)].add(msg.value);
-    return address(this).balance;
+      balance[address(this)].add(msg.value);
+      return address(this).balance;
     }
 
 function withdrawAll() public onlyOwner returns (uint){
-    require(address(this).balance == balance[address(this)]);
+      require(address(this).balance == balance[address(this)]);
 
-    uint oldBalance = address(this).balance;
-    msg.sender.transfer(oldBalance);
+      uint oldBalance = address(this).balance;
+      msg.sender.transfer(oldBalance);
 
-    balance[address(this)].sub(oldBalance);
+      balance[address(this)].sub(oldBalance);
 
-    assert(balance[address(this)] == address(this).balance);
-    return address(this).balance;
+      assert(balance[address(this)] == address(this).balance);
+      return address(this).balance;
     }
 
 function withdrawRewards() public {
 
-    require(balance[msg.sender] > 0);
+      require(balance[msg.sender] > 0);
 
-    uint _balance = balance[msg.sender];
-    uint previousSenderBalance = balance[msg.sender];
+      uint _balance = balance[msg.sender];
+      uint previousSenderBalance = balance[msg.sender];
 
-    msg.sender.transfer(_balance);
-    emit DepositSent(address(this),msg.sender, _balance);
+      msg.sender.transfer(_balance);
+      emit DepositSent(address(this),msg.sender, _balance);
 
-    balance[msg.sender].sub(_balance);
+      balance[msg.sender].sub(_balance);
 
-    assert(balance[msg.sender] == previousSenderBalance.sub(_balance));
+      assert(balance[msg.sender] == previousSenderBalance.sub(_balance));
     }
 
 function getBalance() public view returns (uint) {
 
-    return address(this).balance;
+      return address(this).balance;
     }
 
 function getBetterBalance() public view returns (uint) {
 
-    return balance[msg.sender];
+      return balance[msg.sender];
     }
 
 function getContractAddress() public view returns(address){
 
-    return address(this);
+      return address(this);
     }
 
 function changeOwner(address newOwner) public onlyOwner returns (address){
 
-    owner = newOwner;
-    return owner;
+      owner = newOwner;
+      return owner;
     }
 
 }
